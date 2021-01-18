b16edc79da01e729eaee90a49fba28e607e98e59
Bayja <virlouvetmaxime9@gmail.com>
2021-01-18 19:18:39 +0100

    maj

--- Facturation/Client/Program.cs
+++ Facturation/Client/Program.cs
@@ -1,3 +1,5 @@
+using Facturation.Client.Shared;
+using Facturation.Shared;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +9,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Facturation.Shared;
 
 namespace Facturation.Client
 {
@@ -17,9 +20,31 @@ namespace Facturation.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Services.AddSingleton<IBusinessData>(sp => new BusinessData());
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuratinon;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IBusinessData>(services => new DummyBusinessData());
+            services.AddControllerWithViews();
+            services.AddRazorPages();
     }
+        public void Configure(IApplicationBuilder app, IwebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+        }
 }
